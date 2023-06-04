fdac5cbbd620223f5ac27960072bf6aa00cabda1
taeheon95 <dalo952003@gmail.com>
2023-06-05 01:15:01 +0900

    api 테스트 예시용 추가, api 테스트를 위한 파일 추가 와 수정

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import express, { NextFunction, Request, Response } from "express";
-import bodyParser from "body-parser";
-import resume from "./resume";
-import errorHandler from "./middlewares/errorhandler";
+import app from "./app";
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(errorHandler);
-
-app.use("/resume", resume);
-
-app.post("/", (request: Request, response: Response) => {
-  console.log(request.body);
-  response.send(request.body.data);
-});
-
-app.listen(3000, () => {
-  console.log("server started on port 3000");
-});
+app.listen(3000);
