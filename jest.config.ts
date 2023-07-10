90ca2384f2cfd3e3b37d599e68df62cbd0ac9807
taeheon95 <dalo952003@gmail.com>
2023-07-11 02:05:36 +0900

    세팅 업데이트

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   moduleFileExtensions: ["js", "json", "ts"],
   rootDir: "src",
-  testRegex: [".*\\.test\\.ts"],
+  testRegex: [".*\\.test\\.ts", ".*\\.spec\\.ts"],
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
   },
