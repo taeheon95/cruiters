3ee9093e75c583d146f10d3a028fd25967c295ba
taeheon95 <dalo952003@gmail.com>
2023-07-11 02:06:14 +0900

    객체 인터페이스 추가

---
+++ src/common/Repository.ts
@@ -0,0 +1,7 @@
+export interface Repository<T, ID> {
+  findAll(): Promise<T[]>;
+  findById(id: ID): Promise<T>;
+  create(data: unknown): Promise<T>;
+  update(id: ID, data: unknown): Promise<T>;
+  delete(id: ID): Promise<T>;
+}
