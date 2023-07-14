a4ae42c87fa706141bf3f8926190687af4aa98d3
taeheon95 <dalo952003@gmail.com>
2023-07-14 13:17:10 +0900

    user 에 검색 파람 추가

--- src/main/user/User.controller.ts
+++ src/main/user/User.controller.ts
@@ -1,17 +1,17 @@
-import { UserService } from "./User.service";
-import { UserModel } from "./model/User";
-import { userInputModelValidater } from "./model/User.validator";
-import { numberPipe } from "../common/Validator";
+import { UserService } from './User.service';
+import { UserModel } from './model/User';
+import { userInputModelValidater } from './model/User.validator';
+import { numberPipe } from '../common/Validator';
 
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  public async getUserList(): Promise<UserModel[]> {
+  public async getUserList(query: unknown): Promise<UserModel[]> {
     return await this.userService.getUserList();
   }
 
   public async getUser(param: unknown) {
-    const { id } = numberPipe("id", param);
+    const { id } = numberPipe('id', param);
     return await this.userService.getUser(BigInt(id));
   }
 
@@ -21,13 +21,13 @@ export class UserController {
   }
 
   public async putUser(param: unknown, body: unknown) {
-    const { id } = numberPipe("id", param);
+    const { id } = numberPipe('id', param);
     const user = userInputModelValidater(body);
     return await this.userService.updateUser(BigInt(id), user);
   }
 
   public async deleteUser(param: unknown) {
-    const { id } = numberPipe("id", param);
+    const { id } = numberPipe('id', param);
     return await this.userService.deleteUser(BigInt(id));
   }
 }
--- src/main/user/User.module.ts
+++ src/main/user/User.module.ts
@@ -1,8 +1,8 @@
-import { Express, Router } from "express";
-import { UserRepository, UserRepositoryImpl } from "./User.repository";
-import { UserService, UserServiceImpl } from "./User.service";
-import { UserController } from "./User.controller";
-import { PrismaClient } from "@prisma/client";
+import { Express, Router } from 'express';
+import { UserRepository, UserRepositoryImpl } from './User.repository';
+import { UserService, UserServiceImpl } from './User.service';
+import { UserController } from './User.controller';
+import { PrismaClient } from '@prisma/client';
 
 export default (app: Express, prisma: PrismaClient) => {
   const userRepository: UserRepository = new UserRepositoryImpl(prisma);
@@ -10,33 +10,33 @@ export default (app: Express, prisma: PrismaClient) => {
   const userController = new UserController(userService);
   const router = Router();
 
-  router.get("/:id", async (req, resp) => {
+  router.get('/:id', async (req, resp) => {
     const result = await userController.getUser(req.params);
     resp.json(result);
   });
-  router.get("/", async (_, resp) => {
-    const result = await userController.getUserList();
+  router.get('/', async (req, resp) => {
+    const result = await userController.getUserList(req.query);
     resp.json(result);
   });
-  router.post("/", async (req, resp) => {
+  router.post('/', async (req, resp) => {
     const result = await userController.postUser(req.body);
     resp
-      .setHeader("location", `/user/${result.id.toString()}`)
+      .setHeader('location', `/user/${result.id.toString()}`)
       .status(201)
       .json({
         ...result,
       });
   });
-  router.put("/:id", async (req, resp) => {
+  router.put('/:id', async (req, resp) => {
     const result = await userController.putUser(req.params, req.body);
-    resp.setHeader("location", `/user/${result.id}`).json({
+    resp.setHeader('location', `/user/${result.id}`).json({
       ...result,
     });
   });
-  router.delete("/:id", async (req, resp) => {
+  router.delete('/:id', async (req, resp) => {
     const result = await userController.deleteUser(req.params);
     resp.status(204).json(result);
   });
 
-  app.use("/user", router);
+  app.use('/user', router);
 };
