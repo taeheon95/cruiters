c10ec4695a37da1f8f6a62cb04a8d53a63cefa43
taeheon95 <dalo952003@gmail.com>
2023-07-17 22:03:05 +0900

    readme 수정 및 app.ts 파일 수정

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,35 @@ import errorHandler from "./main/middlewares/errorhandler";
 import UserModule from "./main/user/User.module";
 
 const app: Express = express();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});
 
 app.use(bodyParser.json());
 app.use(errorHandler);
-app.get("/", (req, res) => {
-  res.send("test");
-});
 UserModule(app, prisma);
 
 export default app;
