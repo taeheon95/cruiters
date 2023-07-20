75faf525d69b0ec0379a69e4b8684eb1d055c6da
taeheon95 <dalo952003@gmail.com>
2023-07-20 23:53:27 +0900

    constructor 의 제한에 따른 user 생성 함수 추가 및 테스트 레포지 토리 수정

--- src/main/user/model/User.ts
+++ src/main/user/model/User.ts
@@ -1,8 +1,5 @@
 import { Profile, User } from '@prisma/client';
 
-/**
- * 객체의 불변성을 유지해야 한다.
- */
 export class UserModel {
   private _id: number;
   private _name: string;
@@ -14,23 +11,31 @@ export class UserModel {
   private _profileImage: string;
 
   // 생성자에서는 생성만 처리한다.
-  private constructor(user: User & { profile: Profile }) {
-    this._id = Number(user.id);
-    this._name = user.name;
-    this._birthDate = new Date(user.profile.birthDate);
-    this._countryNumber = user.profile.countryNumber;
-    this._contact = user.profile.contact;
-    this._email = user.profile.email;
-    this._address = user.profile.address;
-    this._profileImage = user.profile.profileImage;
+  private constructor() {
+    this._id = 0;
+    this._name = '';
+    this._birthDate = new Date();
+    this._countryNumber = '';
+    this._contact = '';
+    this._email = '';
+    this._address = '';
+    this._profileImage = '';
   }
 
   get id() {
     return this._id;
   }
 
+  private set id(id: number | bigint) {
+    this._id = Number(id);
+  }
+
   get name() {
     return this._name;
+  }
+
+  private set name(name: string) {
+    this._name = name;
   }
 
   get birthDate() {
@@ -38,24 +43,48 @@ export class UserModel {
     return new Date(this._birthDate);
   }
 
+  private set birthDate(date: Date) {
+    this._birthDate = new Date(date);
+  }
+
   get countryNumber() {
     return this._countryNumber;
+  }
+
+  private set countryNumber(countryNumber: string) {
+    this._countryNumber = countryNumber;
   }
 
   get contact() {
     return this._contact;
   }
 
+  private set contact(contact: string) {
+    this._contact = contact;
+  }
+
   get address() {
     return this._address;
+  }
+
+  private set address(address: string) {
+    this._address = address;
   }
 
   get email() {
     return this._email;
   }
 
+  private set email(email: string) {
+    this._email = email;
+  }
+
   get profileImage() {
     return this._profileImage;
+  }
+
+  private set profileImage(image: string) {
+    this._profileImage = image;
   }
 
   // 유저를 객체를 생성할 때 발생할 수 있는 오류는 스태틱 함수에서 처리한다.
@@ -68,8 +97,42 @@ export class UserModel {
       // TODO: 유저 프로필이 없을 때 오류를 만들어야 함
       throw new Error('');
     }
-    // @ts-ignore
-    return new UserModel(user);
+    const userModel = new UserModel();
+    userModel.id = user.id;
+    userModel.name = user.name;
+    userModel.birthDate = user.profile.birthDate;
+    userModel.countryNumber = user.profile.countryNumber;
+    userModel.contact = user.profile.contact;
+    userModel.email = user.profile.email;
+    userModel.address = user.profile.address;
+    userModel.profileImage = user.profile.profileImage;
+    return userModel;
+  }
+
+  public static createUser(id: number | bigint, user: UserInputModel) {
+    const userModel = new UserModel();
+    userModel.id = Number(id);
+    userModel.name = user.name;
+    userModel.birthDate = user.birthDate;
+    userModel.countryNumber = user.countryNumber;
+    userModel.contact = user.contact;
+    userModel.email = user.email;
+    userModel.address = user.address;
+    userModel.profileImage = user.profileImage;
+    return userModel;
+  }
+
+  public static copy(user: UserModel) {
+    const userModel = new UserModel();
+    userModel.id = user.id;
+    userModel.name = user.name;
+    userModel.birthDate = user.birthDate;
+    userModel.countryNumber = user.countryNumber;
+    userModel.contact = user.contact;
+    userModel.email = user.email;
+    userModel.address = user.address;
+    userModel.profileImage = user.profileImage;
+    return userModel;
   }
 
   toJSON() {
